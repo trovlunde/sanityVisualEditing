4a9f7600fb9fc09cba334de1389b4ec3e5bad1f6
trovlunde <trym.overrein.lunde@bredvid.no>
2024-08-21 14:25:53 +0200

    Fixed one more

--- src/sanity/lib/token.ts
+++ src/sanity/lib/token.ts
@@ -4,7 +4,7 @@ import 'server-only'
 
 import { experimental_taintUniqueValue } from 'react'
 
-export const token = process.env.SANITY_API_READ_TOKEN
+export const token = "skwF6q7R6dC4riVTL1nLBQLd4P1o4hXz93nQwYKVf2Ou5HcBJtPtCOn7qiWUn7EPGAROcVIywLQf7ZNhLetfwzPCbOIR7xLzCiYRdtdT60lNIZHDZLlC9zLg8EDwC0TtGcIJxLUhDuyMlzzU2pOpQNdU7Q5Fte61pgz0AtBwjZpfZavH0vUn"
 
 if (!token) {
   throw new Error('Missing SANITY_API_READ_TOKEN')
