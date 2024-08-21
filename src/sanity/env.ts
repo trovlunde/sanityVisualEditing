eb26dbf290cf522446e0616148eedd1ffebe8780
trovlunde <trym.overrein.lunde@bredvid.no>
2024-08-21 14:21:51 +0200

    Env variables made available

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,15 +1,9 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-07-11'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "testing"
 
-export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
-)
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "yo3cpztx"
 
 export const useCdn = false
 
